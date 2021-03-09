7cd1dab64aa97c749cce3a0ebe893693e56ef962
adityababar07 <adityababar715@gmail.com>
2021-03-09 08:13:52 +0530

    Q34

---
+++ Q34.py
@@ -0,0 +1,25 @@
+def CountYouMe(data):
+    data = f.readlines()
+    count = 0
+    for lines in data:
+        for words in lines.split():
+            #global word
+            words = words.lower()
+            for letter in words:
+                if letter == ".":
+                    words = words.replace(".", "")
+                    f.write(words)
+            if words == "you":
+                print(words)
+                count += 1
+            elif words == "me":
+                print(words)
+                count += 1
+                        
+    print(f"The number of 'You' and 'Me' words is {count}.")
+    return
+
+with open("Notes.txt", "r+") as f:
+    CountYouMe(f)
+                
+                    
